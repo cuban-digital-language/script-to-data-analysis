37a627e01592d083701d940aad1255557fd7617d
yank1 <yancarloglez98@gmail.com>
2022-05-26 15:25:13 -0400

    add data

--- data/telegram.py
+++ data/telegram.py
@@ -6,13 +6,17 @@ def get_text():
     text = []
 
     for filename in os.listdir(os.path.join(os.getcwd(), 'data/telegram')):
-        with open(os.path.join(os.getcwd(), f'data/telegram/{filename}'), 'r') as f: # open in readonly mode
-            try: data = json.load(f)
-            except: continue
+        with open(os.path.join(os.getcwd(), f'data/telegram/{filename}'), 'r') as f:  # open in readonly mode
+            try:
+                data = json.load(f)
+            except:
+                continue
 
             for obj in data:
-                try: text.append(obj['message'])
-                except: pass
+                try:
+                    text.append(obj['message'])
+                except:
+                    pass
 
     return text
 
@@ -21,14 +25,20 @@ def get_details_text():
     text = []
 
     for filename in os.listdir(os.path.join(os.getcwd(), 'data/telegram')):
-        with open(os.path.join(os.getcwd(), f'data/telegram/{filename}'), 'r') as f: # open in readonly mode
-            try: data = json.load(f)
-            except: continue
+        with open(os.path.join(os.getcwd(), f'data/telegram/{filename}'), 'r') as f:  # open in readonly mode
+            try:
+                data = json.load(f)
+            except:
+                continue
 
             for obj in data:
-                try: text.append((obj['message'], obj['date'], None))
-                except: pass
+                try:
+                    text.append((obj['message'], obj['date'], None))
+                except:
+                    pass
 
     return text
 
-get_details_text()
+
+text = get_text()
+print(len(text))
