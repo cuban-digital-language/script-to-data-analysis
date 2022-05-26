f71f2eeaa74f84e0f051108838264d0b2da74d62
yank1 <yancarloglez98@gmail.com>
2022-05-26 16:52:57 -0400

    add data

--- data/telegram.py
+++ data/telegram.py
@@ -40,5 +40,37 @@ def get_details_text():
     return text
 
 
+def splitFile():
+    file1 = []
+    file2 = []
+    for filename in os.listdir(os.path.join(os.getcwd(), 'data/telegram')):
+        with open(os.path.join(os.getcwd(), f'data/telegram/{filename}'), 'r') as f:  # open in readonly mode
+            try:
+                data = json.load(f)
+            except:
+                continue
+
+            for i, obj in enumerate(data):
+                print(f.__sizeof__())
+                try:
+                    if i < len(data) / 2:
+                        file1.append(obj)
+                    else:
+                        file2.append(obj)
+
+                except:
+                    pass
+
+        with open(os.path.join(os.getcwd(), f'data/telegram/{filename}1.json'), 'w') as f:
+            for d in file1:
+                f.write(str(d))
+
+        with open(os.path.join(os.getcwd(), f'data/telegram/{filename}2.json'), 'w') as f:
+            for d in file2:
+                f.write(str(d))
+
+
+splitFile()
+
 text = get_text()
 print(len(text))
